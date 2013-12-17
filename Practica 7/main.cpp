9b50c81fa0498f1082720c5d71b17033bed56465
Francisco López <donflopez@gmail.com>
2013-12-17 09:52:29 +0100

    Initial Commit

---
+++ Practica 7/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Practica 7
+//
+//  Created by Francisco López on 17/12/13.
+//  Copyright (c) 2013 Francisco López. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
